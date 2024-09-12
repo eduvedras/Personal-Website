3aab74973632322d9342f3f5a7d387491f17e34e
eduvedras <edu.vedras@gmail.com>
2024-09-12 12:01:30 +0100

    Projects section

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -13,17 +13,22 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ card }) => {
   let linkcode: string = "";
 
   let skillsPokemon: string[] = ["HTML", "CSS", "JavaScript", "D3.js"];
-  let skillsIVM: string[] = [
-    "HTML",
-    "TailwindCSS",
-    "JavaScript",
-    "React",
-    "Python",
-    "Flask",
+  let skillsBlogAggregator: string[] = [
+    "Go",
     "PostgresSQL",
+    "Bash",
+    "Docker"
   ];
-  let skillsPlanet: string[] = ["HTML", "Javascript", "Three.js"];
+  let skillsQuizzesTutor: string[] = ["Java", "Spring", "Groovy", "TypeScript"];
   let skillsOvercooked: string[] = ["Python", "Pytorch"];
+
+  let image: React.JSX.Element = <img
+    src={card + ".png"}
+    alt="project"
+    className="rounded-xl m-auto"
+    width={348}
+    height={195}
+  />
 
   switch (card) {
     case "Pokemon":
@@ -54,16 +59,16 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ card }) => {
       linkdemo = "https://eduvedras.github.io/Pokemon-Vis/";
       linkcode = "https://github.com/eduvedras/Pokemon-Vis";
       break;
-    case "IVM":
+    case "BlogAggregator":
       headline.push(
         <h1 key={0} className="text-4xl text-gray-50 mx-auto">
-          CRUD app
+          Blog Aggregator
         </h1>
       );
       text =
-        "A three.js project about a spaceship that has to collect debris in the exosphere of a planet. The project implements collisions, a movable camera and constant angular velocity.";
+        "A project that allows users to aggregate all their favorite RSS blogs and feeds in one place. The project includes a scraper that periodically fetches new posts from the blogs in the database. I have currently built the API and a command-line interface tool to simplify interaction with this API. I also plan to build a graphical user interface in the future, so for now, the demo button links to the CLI repo. The project was built in Go and I applied several of my skills, including setting up CI/CD pipelines using GitHub Actions, deploying on Google Cloud, implementing parallel programming to build the scraper that retrieves posts from each blog, and handling user authentication, database migrations, and deployments.";
 
-      skillsIVM.forEach((element) => {
+      skillsBlogAggregator.forEach((element) => {
         skills.push(
           <div
             key={element}
@@ -74,19 +79,19 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ card }) => {
         );
       });
 
-      linkdemo = "https://ivm-frontend.netlify.app";
-      linkcode = "https://github.com/eduvedras/IVM-Database";
+      linkdemo = "https://github.com/eduvedras/Blog-Aggregator-CLI";
+      linkcode = "https://github.com/eduvedras/Blog-Aggregator";
       break;
-    case "Planet":
+    case "QuizzesTutor":
       headline.push(
         <h1 key={0} className="text-4xl text-gray-50 mx-auto">
-          Space Debris
+          Quizzes Tutor
         </h1>
       );
       text =
-        "A three.js project about a spaceship that has to collect debris in the exosphere of a planet. The project implements collisions, a movable camera and constant angular velocity.";
+        "Quizzes Tutor is a platform used at my college (IST) that allows professors to create quizzes for students to answer. It has several features related to quiz statistics. We implemented one of these features: a functionality that shows students all their incorrect answers in chronological order. For this project, we used Java and Spring for the backend, Groovy for testing, and TypeScript for the frontend.";
 
-      skillsPlanet.forEach((element) => {
+      skillsQuizzesTutor.forEach((element) => {
         skills.push(
           <div
             key={element}
@@ -97,8 +102,8 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ card }) => {
         );
       });
 
-      linkdemo = "https://eduvedras.github.io/SpaceDebris/";
-      linkcode = "https://github.com/eduvedras/SpaceDebris";
+      linkdemo = "https://quizzes-tutor.tecnico.ulisboa.pt/student/dashboard";
+      linkcode = "https://github.com/eduvedras/Projeto-ES-21-22";
       break;
     case "Overcooked":
       headline.push(
@@ -107,7 +112,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ card }) => {
         </h1>
       );
       text =
-        "A multi-agent reinforcement learning (MARL) project to train agents to play the game Overcooked. The agents are trained using Q-Learning and SARSA.";
+        "A multi-agent reinforcement learning (MARL) project to train two agents to play the game Overcooked. The agents are trained using classic reinforcement learning algorithms, Q-Learning and SARSA.";
 
       skillsOvercooked.forEach((element) => {
         skills.push(
@@ -129,13 +134,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ card }) => {
     <div className="flex flex-col border-teal-500 border rounded-xl m-auto p-8 gap-8 w-2/3 hover:shadow-lg hover:shadow-teal-600 duration-300 transition ease-in-out hover:-translate-y-1">
       <div className="flex flex-col">{headline}</div>
 
-      <img
-        src={card + ".png"}
-        alt="project"
-        className="rounded-xl m-auto"
-        width={348}
-        height={195}
-      />
+      {card != "BlogAggregator" ? image : ""}
 
       <span className="text-lg text-gray-400"> {text}</span>
 
@@ -151,7 +150,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ card }) => {
           className="cursor-pointer absolute right-5 text-teal-600 scale-[2.9] hover:text-gray-200 hover:scale-[3.19] duration-300"
         />
       </div>
-    </div>
+    </div >
   );
 };
 
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -5,8 +5,8 @@ function Projects() {
     <section className="w-screen flex flex-col gap-12" id="projects">
       <h1 className="mx-auto text-6xl text-teal-500">Projects</h1>
       <div className="grid grid-rows-2 grid-cols-2 gap-8 m-auto">
-        <ProjectCard card="IVM" />
-        <ProjectCard card="Planet" />
+        <ProjectCard card="BlogAggregator" />
+        <ProjectCard card="QuizzesTutor" />
         <ProjectCard card="Overcooked" />
         <ProjectCard card="Pokemon" />
       </div>
